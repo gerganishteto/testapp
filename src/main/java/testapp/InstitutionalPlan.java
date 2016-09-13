8007d62d3b7db8627e8456ca58c16411c9395fb7
gerganishteto <gerganishteto@gmail.com>
2016-09-13 16:34:27 +0300

    Initial

---
+++ src/main/java/testapp/InstitutionalPlan.java
@@ -0,0 +1,8 @@
+package testapp;
+
+class  InstitutionalPlan extends Plan{  
+	   //@override  
+	    public void getRate(){   
+	        rate=5.50;  
+	   }
+}
---
+++ src/test/java/testapp/DomesticPlanTest.java
@@ -0,0 +1,14 @@
+package testapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DomesticPlanTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
